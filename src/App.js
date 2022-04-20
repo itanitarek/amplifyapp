a8dc3af4730b29cc1c611eb117eedb4f888c7c74
itanitarek <104010977+itanitarek@users.noreply.github.com>
2022-04-20 14:48:51 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Love You Karuna </h1>
+        <h1>My sweet Love , Love you sooo much ***</h1>
       </header>
     </div>
   );
