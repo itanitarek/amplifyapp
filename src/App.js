1e8c238013222518905c5e7395ab9174666e9f28
itanitarek <104010977+itanitarek@users.noreply.github.com>
2022-04-20 09:34:53 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+        <h1>Ya Hamoudi </h1>
       </header>
     </div>
   );
